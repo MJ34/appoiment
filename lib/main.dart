e02dfe17b5726a8e7bfadeb631a73a19f1316daf
kasur.rusak404@gmail.com <1631Nz94@>
2023-01-03 11:12:14 +0700

    start 1 project booking

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:booking/main_layout.dart';
+import 'package:booking/screens/auth_page.dart';
+import 'package:booking/utils/config.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  //this is for push navigator
+  static final navigatorKey = GlobalKey<NavigatorState>();
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      navigatorKey: navigatorKey,
+      title: 'Doctor Apps',
+      theme: ThemeData(
+        //pre-define input decoration
+        inputDecorationTheme: const InputDecorationTheme(
+          focusColor: Config.primaryColor,
+          border: Config.outlinedBorder,
+          focusedBorder: Config.focusBorder,
+          errorBorder: Config.errorBorder,
+          enabledBorder: Config.outlinedBorder,
+          floatingLabelStyle: TextStyle(color: Config.primaryColor),
+          prefixIconColor: Colors.black38,
+        ),
+        scaffoldBackgroundColor: Colors.white,
+        bottomNavigationBarTheme: BottomNavigationBarThemeData(
+          backgroundColor: Config.primaryColor,
+          selectedItemColor: Colors.white,
+          showSelectedLabels: true,
+          showUnselectedLabels: false,
+          unselectedItemColor: Colors.grey.shade700,
+          elevation: 10,
+          type: BottomNavigationBarType.fixed,
+        ),
+      ),
+      initialRoute: '/',
+      routes: {
+        //this is initial route of the app
+        //which is auth page (login and sign up)
+        '/': (context) => const AuthPage(),
+        //this is for main layout after login
+        'main': (context) => const MainLayout(),
+      },
+    );
+  }
+}
