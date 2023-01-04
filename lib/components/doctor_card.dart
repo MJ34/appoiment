3612eb61e57076deeb5b039a143919a1c8b7c2fc
kasur.rusak404@gmail.com <1631Nz94@>
2023-01-04 14:22:12 +0700

    design Appoiment dan Dokter Card done!

--- lib/components/doctor_card.dart
+++ lib/components/doctor_card.dart
@@ -1,3 +1,4 @@
+import 'package:booking/utils/config.dart';
 import 'package:flutter/material.dart';
 
 class DoctorCard extends StatefulWidget {
@@ -10,6 +11,67 @@ class DoctorCard extends StatefulWidget {
 class _DoctorCardState extends State<DoctorCard> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    Config().init(context);
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      height: 150,
+      child: GestureDetector(
+        child: Card(
+          elevation: 5,
+          color: Colors.white,
+          child: Row(
+            children: [
+              SizedBox(
+                width: Config.widthSize * 0.33,
+                child: Image.asset(
+                  'assets/dokter2.png',
+                  fit: BoxFit.fill,
+                ),
+              ),
+              Flexible(
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children:  <Widget>[
+                      const Text(
+                        'Dr Sella',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const Text(
+                        'Cardiology',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.normal,
+                        ),
+                      ),
+                      const Spacer(),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: const <Widget>[
+                          Icon(Icons.star_border, color: Colors.yellow, size: 16,),
+                          Spacer(flex: 1,),
+                          Text('4.5'),
+                          Spacer(flex: 1,),
+                          Text('Reviews'),
+                          Spacer(flex: 1,),
+                          Text('(20)'),
+                          Spacer(flex: 7,),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        onTap: () {},
+      ),
+    );
   }
 }
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,4 +1,4 @@
-import 'package:booking/screens/appointment_page.dart';
+import 'package:booking/components/doctor_card.dart';
 import 'package:booking/utils/config.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -48,88 +48,96 @@ class _HomePageState extends State<HomePage> {
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
         child: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: const <Widget>[
-                  Text(
-                    'Mujiono',
-                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(
-                    child: CircleAvatar(
-                      radius: 30,
-                      backgroundImage: AssetImage('assets/profile1.jpg'),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const <Widget>[
+                    Text(
+                      'Mujiono',
+                      style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
                     ),
-                  )
-                ],
-              ),
-              Config.spaceMedium,
-              //category listing
-              const Text(
-                'Category',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
+                    SizedBox(
+                      child: CircleAvatar(
+                        radius: 30,
+                        backgroundImage: AssetImage('assets/profile1.jpg'),
+                      ),
+                    )
+                  ],
                 ),
-              ),
-              Config.spaceSmall,
-              SizedBox(
-                height: Config.heightSize * 0.05,
-                child: ListView(
-                  scrollDirection: Axis.horizontal,
-                  children: List<Widget>.generate(medCat.length, (index) {
-                    return Card(
-                      margin: const EdgeInsets.only(right: 20),
-                      color: Config.primaryColor,
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 15,
-                          vertical: 10
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          children: <Widget>[
-                            FaIcon(medCat[index]['icon'],
-                              color: Colors.white,
-                            ),
-                            const SizedBox(width: 20,),
-                            Text(medCat[index]['category'],
-                              style: const TextStyle(
-                                fontSize: 16,
+                Config.spaceMedium,
+                //category listing
+                const Text(
+                  'Category',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Config.spaceSmall,
+                SizedBox(
+                  height: Config.heightSize * 0.05,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: List<Widget>.generate(medCat.length, (index) {
+                      return Card(
+                        margin: const EdgeInsets.only(right: 20),
+                        color: Config.primaryColor,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 15,
+                            vertical: 10
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: <Widget>[
+                              FaIcon(medCat[index]['icon'],
                                 color: Colors.white,
                               ),
-                            ),
-                          ],
+                              const SizedBox(width: 20,),
+                              Text(medCat[index]['category'],
+                                style: const TextStyle(
+                                  fontSize: 16,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
-                      ),
-                    );
+                      );
+                    }),
+                  ),
+                ),
+                Config.spaceSmall,
+                const Text(
+                  'Appointment Today',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Config.spaceSmall,
+                //display appointment card here
+                const AppointmentCard(),
+                Config.spaceSmall,
+                const Text(
+                  'Top Doctors',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Config.spaceSmall,
+                Column(
+                  children: List.generate(10, (index) {
+                    return const DoctorCard();
                   }),
-                ),
-              ),
-              Config.spaceSmall,
-              const Text(
-                'Appointment Today',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Config.spaceSmall,
-              //display appointment card here
-              const AppointmentCard(),
-              Config.spaceSmall,
-              const Text(
-                'Top Doctors',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ],
+                )
+              ],
+            ),
           ),
         ),
       ),
